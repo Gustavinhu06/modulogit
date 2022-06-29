f45bfd222f0bacee71fcba8e1a856208c50c0e72
Gustavo06 <gugu.r.almeida2010@gmail.com>
2022-06-29 18:32:30 -0300

    continuação da matéria Javascrpit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("Titulo").innerHTML = "Opa, TA SUAVE?";
+document.write ("IAE, Monark aqui");
+
+
+var hora = 12;
+
+if (hora == 12 || hora == 18 ) {
+    console.log("Ta na hora do rush");
+}
